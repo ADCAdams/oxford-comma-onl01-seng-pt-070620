76558401719b6712ec756ae9283a121e3c84d7d4
Andy Adams <andrewdcadams@gmail.com>
2020-07-08 20:02:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,4 @@
 def oxford_comma(array)
-  array[array.length - 1] = "and" + array[array.length - 1]
-  puts array
-  puts("hahahaha")
+  array[array.length - 1] = "and " + array[array.length - 1]
+  array.joing()
 end
