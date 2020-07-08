028b2706629c88060f5af086eb45ea11ec8a0dff
Andy Adams <andrewdcadams@gmail.com>
2020-07-08 20:08:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 def oxford_comma(array)
   case array.length
-  when
-  array[array.length - 1] = "and " + array[array.length - 1]
-  array.join(", ")
+  when 1 
+      return array
+    when 2 
+      array.join(" and ")
+      return array
+    else
+    array[array.length - 1] = "and " + array[array.length - 1]
+    array.join(", ")
+    return array
+  end
 end
